1eba6f6cad1f36bb4e0033ba2e603301e83e3ee4
goggalor1954 <goggalor1954@gmail.com>
2022-09-07 21:26:13 -0400

    Base depth charge game is done. I can revisit this later to make the sub move if I get around to it.

--- COBOL/1 Depth Charge/DepthCharge.cob
+++ COBOL/1 Depth Charge/DepthCharge.cob
@@ -4,7 +4,7 @@
       *-----------------------
        AUTHOR. James Roesemann.
       * MODIDFIED FORM A PROGRAM WRITTEN BY Dana Noftle.
-      DATE-WRITTEN. September 9th 2022
+      DATE-WRITTEN. September 7th 2022
 
       *--------------------
        ENVIRONMENT DIVISION.
@@ -25,30 +25,10 @@
       *   
       *******************************************************
        INPUT-OUTPUT SECTION.
-      * only enable the file control if you want to make it compatibl
-      *with JCl
-      *FILE-CONTROL.
-      *     SELECT GAME-RECORDS ASSIGN TO GAMEREC.
-      *    ORGANIZATION IS LINE SEQUENTIAL
-      *    ACCESS IS SEQUENTIAL.
 
       *-------------
        DATA DIVISION.
       *-------------
-
-      * FILE SECTION.
-      * FD  GAME-RECORDS
-      *     RECORD CONTAINS 80 CHARACTERS
-      *     RECORDING MODE IS F.
-      * 01  GAME-BOARD-O.
-      *     02  SHOTS-O PIC 9(3).
-      *     02  DIMENSION-O PIC 9(03).
-      *     02  TRIES-O PIC 9(3).
-
-      * 01  SUB-LOCATION-O.
-      *     02 SUB-X-POS-O PIC 9(3).
-      *     02 SUB-Y-POS-O PIC 9(3).
-      *     02 SUB-Z-POS-O PIC 9(3).
 
        WORKING-STORAGE SECTION.
 
@@ -73,28 +53,32 @@
            02  SHOT-Y PIC 9(3).
            02  SHOT-Z PIC 9(3).
 
+       01  PLAYER.
+           02 PLAY-ANSWER PIC X(1).
+
 
       *------------------
        PROCEDURE DIVISION.
       *------------------
+      *ACCEPT A 3 DIGIT NUMBER TO USE AS THE DIMENSTION OF THE GAME
+      *USE THIS VALUE TO ALLOCATE SHOTS RELATIVE TO THE DIMENSION SIZE
        GET-DIMENSION.
            DISPLAY "ENTER A NUMBER BETWEEN 1 AND 100."
            ACCEPT DIMENSION.
            IF DIMENSION IS > 99 OR < 2
-           DISPLAY "YOU HAVE ENTERED AN INCORRECT VALUE"
+           DISPLAY "YOU HAVE ENTERED AN INCORRECT VALUE. TRY AGAIN."
            GO TO GET-DIMENSION
            ELSE 
-      *I DON'T REMBER WHERE I FOUND THIS FUNCTION TO DETERMINE 
-      *SHOTS MAYBE REPLACE IT LATER
            COMPUTE SHOTS = FUNCTION LOG(DIMENSION) / FUNCTION LOG(2).
            ADD 1 TO SHOTS.
 
        GAME-START.
            DISPLAY '            DEPTH CHARGE GAME'.
+           DISPLAY 'DIMENSION OF SEARCH AREA ' DIMENSION.
            DISPLAY 'YOU ARE THE CAPTIN OF THE DESTROYER USS DIGITAL'
            DISPLAY 'AN ENEMY SUB HAS BEEN CAUSING YOU TROUBLE. YOUR'
            DISPLAY 'MISSION IS TO DESTROY IT.'
-           DISPLAY 'YOU HAVE ' SHOTS ' SHOTS'
+           DISPLAY 'YOU HAVE ' SHOTS ' SHOTS.'
            DISPLAY 'SPECIFY DEPTH CHARGE EXPLOSION POINT WITH A TRIO'
            DISPLAY 'OF NUMBERS -- THE FIRST TWO ARE THE SURFACE'
            DISPLAY 'CORDINATES. THE THIRD IS THE DEPTH.'
@@ -107,36 +91,38 @@
            COMPUTE SUB-X-POS = DIMENSION * FUNCTION RANDOM.
            COMPUTE SUB-Y-POS = DIMENSION * FUNCTION RANDOM.
            COMPUTE SUB-Z-POS = DIMENSION * FUNCTION RANDOM.
-      *TESTING
-           DISPLAY 'SUB X ' SUB-X-POS 
-           DISPLAY 'SUB Y ' SUB-Y-POS 
-           DISPLAY 'SUB Z ' SUB-Z-POS 
-
            GO TO EVALUATE-INPUT.
 
        GIVEN-WRONG-INPUT.
            DISPLAY 'YOU HAVE ENTERED AN INCORRECT VALUE. TRY AGAIN.'.
 
+      *ACCEPT USER INPUT AND EVALUATE IF IS CAN BE USED FOR CORDINATES.
        EVALUATE-INPUT.
            DISPLAY 'ENTER THREE NUMBERS BETWEEN 1 AND ' DIMENSION'.'
-           ACCEPT T-X-POS.
-           IF T-X-POS  <  2 OR > DIMENSION
-           GO TO GIVEN-WRONG-INPUT
-           ELSE
+           DISPLAY 'TARGET POSITION Y:' WITH NO ADVANCING
       *     ACCEPT T-Y-POS FROM USER-INPUT.
            ACCEPT T-Y-POS.
            IF T-Y-POS  <  2 OR > DIMENSION
            GO TO GIVEN-WRONG-INPUT
            ELSE
+      *     ACCEPT T-X-POS FROM USER-INPUT.     
+           DISPLAY 'TARGET POSITION X:' WITH NO ADVANCING
+           ACCEPT T-X-POS.
+           IF T-X-POS  <  2 OR > DIMENSION
+           GO TO GIVEN-WRONG-INPUT
+           ELSE           
       *     ACCEPT T-Z-POS FROM USER-INPUT.
+           DISPLAY 'TARGET POSITION Z:' WITH NO ADVANCING
            ACCEPT T-Z-POS.
            IF T-Z-POS  <  2 OR > DIMENSION
            GO TO GIVEN-WRONG-INPUT
            ELSE GO TO NEXT-TURN.
 
+      * EVALUATE DEPTH CHARGE TARGET RELATIVE TO SUB LOCATION 
        NEXT-TURN.
            ADD 1 TO TRIES
-           DISPLAY 'TRIAL #' SHOTS ' ' T-X-POS ' ' T-Y-POS ' ' T-Z-POS
+           DISPLAY 'TRIAL #' SHOTS ' X:' T-X-POS ' Y:' T-Y-POS 
+           ' Z:' T-Z-POS
            COMPUTE SHOT-X = FUNCTION ABS(SUB-X-POS - T-X-POS)
            COMPUTE SHOT-Y = FUNCTION ABS(SUB-Y-POS - T-Y-POS)
            COMPUTE SHOT-Z = FUNCTION ABS(SUB-Z-POS - T-Z-POS)
@@ -144,12 +130,9 @@
            IF SHOT-X IS EQUAL TO 0
            GO TO ENEMY-HIT
            ELSE
-      *DO SOME MATH HERE. PROBALY TO ANOTHER PARAGRAPH WITH SOANER STUFF
-      *NEED TO FIGURE OUT THIS GUS SPAGETTI CODE. JUMPS ARROUND ALOT.
            GO TO SONAR.
-       
-      * I NEED TO RE WROK THE X AND Y CORDS. IT DOSENT FIT WITH HOW YOU
-      * THINK OF IT ON A MAP. Z IS FINE.
+           
+      * DISPLAY THE GENRAL LOCATION OF THE SUB RELATIVE TO THE TRGET.
        SONAR.
            DISPLAY 'SONAR REPORTS SHOT WAS ' WITH NO ADVANCING 
            IF T-Y-POS > SUB-Y-POS THEN DISPLAY 'NORTH' 
@@ -163,35 +146,43 @@
            WITH NO ADVANCING
            END-IF.
            IF (T-X-POS NOT = SUB-Y-POS AND T-X-POS NOT = SUB-X-POS)
-           THEN DISPLAY ' AND ' WITH NO ADVANCING
-           IF T-Z-POS > SUB-Z-POS THEN DISPLAY 'TOO LOW' 
+           THEN DISPLAY ' AND' WITH NO ADVANCING
+           END-IF.
+      * THIS PART SEEMS CONFUUSING. I MUIIGHT CHAGE IT SO THAT ZERO
+      *STARTS AT THE BOTTOM AND NOT THE TOP.     
+           IF T-Z-POS < SUB-Z-POS THEN DISPLAY ' TOO LOW' 
            WITH NO ADVANCING
-           ELSE IF T-Z-POS < SUB-Z-POS THEN DISPLAY 'TOO HIGH' 
+           ELSE IF T-Z-POS > SUB-Z-POS THEN DISPLAY ' TOO HIGH' 
            WITH NO ADVANCING
-           ELSE IF T-Z-POS IS EQUAL TO SUB-Z-POS THEN DISPLAY 'DEPTH OK'
+           ELSE IF T-Z-POS IS EQUAL TO SUB-Z-POS 
+           THEN DISPLAY ' DEPTH OK'
            WITH NO ADVANCING
            END-IF.
            DISPLAY '.'
            SUBTRACT 1 FROM SHOTS.
+      *testing
+           DISPLAY TRIES     
            IF SHOTS IS EQUAL TO 0 THEN GO TO ABANDON-SHIP
            ELSE GO TO EVALUATE-INPUT.
-
 
        ABANDON-SHIP.
            DISPLAY 'YOU HAVE BEEN TORPEDOED! ABANDON SHIP!'
            DISPLAY 'THE SUBMARINE WAS AT ' SUB-X-POS WITH NO ADVANCING
            DISPLAY ' ' SUB-Y-POS ' ' SUB-Z-POS
-           GO TO GAME-STOP.
-
+           GO TO NEW-GAME.
 
        ENEMY-HIT.
            DISPLAY 'B O O M !  YOU FOUND IT IN ' TRIES ' TRIES.'.
+           
+       NEW-GAME.
+           MOVE 000 TO TRIES.    
+           DISPLAY 'DO YOU WANT TO PLAY AGAIN? (Y OR N):' 
+           WITH NO ADVANCING
+           ACCEPT PLAY-ANSWER
+           IF PLAY-ANSWER IS EQUAL TO 'Y' OR 'y' GO TO GET-DIMENSION
+           ELSE IF PLAY-ANSWER IS EQUAL TO 'N' OR 'n' GO TO GAME-STOP
+           ELSE DISPLAY 'I''M SORRY. I DIDN''T UNDERSTAND THAT.'
+           GO TO NEW-GAME.
 
        GAME-STOP.
            STOP RUN.           
-
-
-
-      * i need to fix GAMEREC. does not curently exist. 
-      * need to create GAMEREC if it dosent Exist.
-      * need to delete it when the game is over
